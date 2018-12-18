f4d43a44876028e5525f27d39051c7b7bc66de01
Thomas Bale <thomasbale@gmail.com>
2018-12-18 16:10:53 +0000

    Implimented supporting Frameworks
    Including ARuCO as part of OpenCV installation

--- TaskManagAR/TaskManagAR/ViewController.swift
+++ TaskManagAR/TaskManagAR/ViewController.swift
@@ -17,17 +17,15 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        print("\(OpenCVWrapper.openCVVersionString())")
+        
+        
         // Set the view's delegate
         sceneView.delegate = self
         
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
     }
     
     override func viewWillAppear(_ animated: Bool) {
