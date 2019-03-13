2c81a503fed1a5f5bb880a3a4f5ad2c141a139fc
Thomas Bale <thomasbale@gmail.com>
2019-03-13 14:51:32 +0000

    New developer certificate
    Moved to apple developer programme and added new provisioning certificate

---
+++ TaskManagAR/TaskManagAR/ViewControl/EventCell.swift
@@ -0,0 +1,33 @@
+//
+//  EventCell.swift
+//  TaskManagAR
+//
+//  Created by Thomas Bale on 13/03/2019.
+//  Copyright © 2019 Thomas Bale. All rights reserved.
+//
+
+import UIKit
+
+class EventCell: UITableViewCell {
+    
+    //MARK: Properties
+
+    @IBOutlet weak var EventName: UIView!
+    
+    func createEvent(Name: String){
+        EventName.
+        return
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
